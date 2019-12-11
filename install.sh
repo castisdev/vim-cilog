e32928884aea3b296d0bd3ae1adb0e72a66d5500
pinkcubrick <pinkcubrick@gmail.com>
2019-12-11 14:12:59 +0900

    설치시 디렉토리 메시지

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
-VIMDIR=`vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015' `/syntax
+VIMDIR=`vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015' `
+if [ -z "$VIMDIR" ]
+then
+    echo "failed to get vim directory"
+    exit 1
+fi
+VIMDIR=$VIMDIR/syntax
+echo "install to " $VIMDIR
+
 sudo cp cilog.vim $VIMDIR
 mkdir -p ~/.vim/ftdetect
 echo "au BufNewFile,BufRead *.log setf cilog" > ~/.vim/ftdetect/cilog.vim
